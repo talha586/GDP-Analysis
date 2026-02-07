a182277edf04802d230b1fefbbcebcc5cac8bdc5
Muhammad Talha <muhammadtalhaafzal951@gmail.com>
2026-02-07 19:07:40 +0500

    Perform Statistical Analysis

--- data_process.py
+++ data_process.py
@@ -1,3 +1,4 @@
+from functools import reduce
 
 def Clean_CSV(text_csv):
     cleaned_csv_file=map(lambda row:[cell if cell!='' else 0.0 for cell in row], text_csv)
@@ -11,4 +12,27 @@ def Filter_CSV_Country(clean_text_csv,CountryName):
     filter_csv_file=filter(lambda x: str(x[0]).strip() == CountryName ,clean_text_csv)
     return tuple(filter_csv_file)
 
-def Find_Sum():
+def Find_Sum(filter_clean_text_csv,start_index,end_index):
+    total_gdp=0.0
+
+    for row in filter_clean_text_csv:
+        year_range_value=row[start_index:end_index+1]
+        row_sum=sum(float(val) for val in year_range_value)
+        total_gdp+=row_sum
+
+    return float(total_gdp)   
+
+def Statistical_Analysis(operation,filter_clean_text_csv,start_index,end_index):
+
+    Sum_GDP=Find_Sum(filter_clean_text_csv,start_index,end_index)
+    difference=end_index-start_index+1
+    if operation.lower()=="sum":
+        return float(Sum_GDP)
+
+    elif operation.lower()=="average":
+        return Sum_GDP/difference
+
+    else:
+        print("Not a valid Operation")
+        return 0.0
+    
--- main.py
+++ main.py
@@ -13,7 +13,8 @@ header_text=text_csv[0]
 data_rows=text_csv[1:]
 
 RegionYear=str(text_json['year'])
-
+start_year="1960"
+operation=str(text_json['operation'])
 clean_text_csv = data_process.Clean_CSV(data_rows)
 
 #Filter For Region
@@ -28,13 +29,17 @@ clean_text_csv = data_process.Clean_CSV(data_rows)
 
 
 #Filter For Country
+
+start_index=header_text.index(start_year)
+end_index=header_text.index(RegionYear)
+
 filter_clean_text_csv=data_process.Filter_CSV_Country(clean_text_csv,CountryName)
 
-if not filter_clean_text_csv:
-    print("No record Found")
-else:
-    for row in filter_clean_text_csv:
-        pprint(dict(zip(header_text,row))) 
+# if not filter_clean_text_csv:
+#     print("No record Found")
+# else:
+#     for row in filter_clean_text_csv:
+#         pprint(dict(zip(header_text,row))) 
 
-total_sum=data_process.Find_Sum(filter_clean_text_csv)
-     
+result=data_process.Statistical_Analysis(operation,filter_clean_text_csv,start_index,end_index)
+print(f"Result of {operation} of GDP on {CountryName} is: {result}")
